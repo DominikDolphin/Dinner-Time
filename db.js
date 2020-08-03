127514e6070ceafa92ab7c704c1981d3322c4803
DominikDolphin <dominik.thibaudeau@gmail.com>
2020-08-03 19:43:59 -0400

    Password are now hashed using bcrypt

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const bcrypt = require('bcryptjs');
 let Schema = mongoose.Schema;
 /*
 let studentSchema = new Schema({
@@ -54,7 +54,7 @@ module.exports.initialize = function() {
     });
 }
 
-
+/*
 module.exports.addStudent = function(data) {
     return new Promise((resolve, reject) => {
         //prep the incoming data 
@@ -82,6 +82,54 @@ module.exports.addStudent = function(data) {
         });
     });
 }
+*/
+module.exports.addStudent = function(data) {
+    return new Promise((resolve, reject) => {
+        //prep the incoming data 
+
+        //see if it has been "checked"
+        data.international = (data.international) ? true : false;
+
+        //set data to null if an empty string ""
+
+
+        //for-in loops the variable that we loop with is a copy. 
+        for (var formEntry in data) {
+            if (data[formEntry] == "") //lets just say this works the way we want
+                data[formEntry] = null;
+        }
+
+        //add data to based off local Student Collection
+        //local only so data is not pushed to mongo
+        //NOTE: only works if the field names are the same. "Name" vs "name" doesn't work. 
+        var newStudent = new Students(data); //copy constructor   
+        //add hashed password
+        bcrypt.genSalt(10) // Generate a "salt" using 10 rounds
+            .then(salt => bcrypt.hash(newStudent.password, salt)) // use the generated "salt" to encrypt the password: "myPassword123"
+            .then(hash => { //returns encrypted password
+                // TODO: Store the resulting "hash" value in the DB
+                newStudent.password = hash;
+                //tries to save entry in our database
+                newStudent.save((err) => {
+                    if (err) {
+                        console.log("Woopsie there was an error: " + err);
+                        reject(err);
+                    } else {
+                        console.log("Saved that student: " + data.firstName);
+                        resolve();
+                    }
+                });
+            })
+            .catch(err => {
+                console.log(err); // Show any errors that occurred during the process
+                reject("Hashing Error");
+            });
+
+
+    });
+}
+
+
 
 
 
@@ -112,7 +160,23 @@ module.exports.getAllCarsP = () => {
     });
 }
 
-
+module.exports.getStudentsByEmail = function(inEmail) {
+    return new Promise((resolve, reject) => {
+        //email has to be spelled the same as in the data base
+        Students.find({ email: inEmail }) //gets all and returns an array. Even if 1 or less entries
+            .exec() //tells mongoose that we should run this find as a promise.
+            .then((returnedStudents) => {
+                if (returnedStudents.length != 0)
+                //resolve(filteredMongoose(returnedStudents));
+                    resolve(returnedStudents.map(item => item.toObject()));
+                else
+                    reject("No Students found");
+            }).catch((err) => {
+                console.log("Error Retriving students:" + err);
+                reject(err);
+            });
+    });
+}
 
 
 
